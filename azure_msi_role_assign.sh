1aa8a5c905a9b93377abf81d4ed542bb757591db
cpv0310 <cpv0310@live.com>
2020-05-07 15:27:01 -0500

    Update azure_msi_role_assign.sh

--- azure_msi_role_assign.sh
+++ azure_msi_role_assign.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-export SUBSCRIPTIONID=<SubscriptionID>
-export RESOURCEGROUPNAME=<rg-name>
-export STORAGEACCOUNTNAME=<sa-name>
-export ASSUMER_OBJECTID=<envName-Assumer-ObjectID>
-export DATAACCESS_OBJECTID=<envName-DataAccess-ObjectID>
-export LOGGER_OBJECTID=<envName-Logger-ObjectID>
-export RANGER_OBJECTID=<envName-Ranger-ObjectID>
+export SUBSCRIPTIONID="<add subscription id>"
+export RESOURCEGROUPNAME="<add resource group name>"
+export STORAGEACCOUNTNAME=$(az storage account list -g $RESOURCEGROUPNAME|jq '.[]|.name'| tr -d '"')
+export ASSUMER_OBJECTID=$(az identity list -g $RESOURCEGROUPNAME|jq '.[]|{"name":.name,"principalId":.principalId}|select(.name | test("AssumerIdentity"))|.principalId'| tr -d '"')
+export DATAACCESS_OBJECTID=$(az identity list -g $RESOURCEGROUPNAME|jq '.[]|{"name":.name,"principalId":.principalId}|select(.name | test("DataAccessIdentity"))|.principalId'| tr -d '"')
+export LOGGER_OBJECTID=$(az identity list -g $RESOURCEGROUPNAME|jq '.[]|{"name":.name,"principalId":.principalId}|select(.name | test("LoggerIdentity"))|.principalId'| tr -d '"')
+export RANGER_OBJECTID=$(az identity list -g $RESOURCEGROUPNAME|jq '.[]|{"name":.name,"principalId":.principalId}|select(.name | test("RangerIdentity"))|.principalId'| tr -d '"')
+
+
 # Assign Managed Identity Operator role to the assumerIdentity principal at subscription scope
 az role assignment create --assignee $ASSUMER_OBJECTID --role 'f1a07417-d97a-45cb-824c-7a7467783830' --scope "/subscriptions/$SUBSCRIPTIONID"
 # Assign Virtual Machine Contributor role to the assumerIdentity principal at subscription scope
 az role assignment create --assignee $ASSUMER_OBJECTID --role '9980e02c-c2be-4d73-94e8-173b1dc7cf3c' --scope "/subscriptions/$SUBSCRIPTIONID"
+
 # Assign Storage Blob Data Contributor role to the loggerIdentity principal at logs filesystem scope
 az role assignment create --assignee $LOGGER_OBJECTID --role 'ba92f5b4-2d11-453d-a403-e96b0029c9fe' --scope "/subscriptions/$SUBSCRIPTIONID/resourceGroups/$RESOURCEGROUPNAME/providers/Microsoft.Storage/storageAccounts/$STORAGEACCOUNTNAME/blobServices/default/containers/logs"
 # Assign Storage Blob Data Owner role to the dataAccessIdentity principal at logs/data filesystem scope
